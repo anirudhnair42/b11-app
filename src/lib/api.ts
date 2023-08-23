28df6ce8fede4874b0823a42487aaf294212d4f3
Anirudh Nair <anirudhnair42@gmail.com>
2023-08-23 01:47:57 -0700

    AN-004: Harcoding the api route

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: "https://b11-api-57a95f225119.herokuapp.com/",
 });
 
 api.interceptors.request.use(
